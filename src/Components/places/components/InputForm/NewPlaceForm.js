6d9f614b1d249c8f77f72a092c36376e0b1d388f
Chinmay Mulay <myname.chinmay.18@gmail.com>
2022-08-04 20:40:05 +0530

    Default validation added to the new place form

--- src/Components/places/components/InputForm/NewPlaceForm.js
+++ src/Components/places/components/InputForm/NewPlaceForm.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React , {useState} from "react";
 import {Form , Button,FloatingLabel} from 'react-bootstrap';
 
 import './newPlaceForm.css';
 
 const NewPlaceForm = () => {
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      setValidated(true);
+    };
+
+ 
+
+    
+
+  
     return (
 //         <form>
 //         <div className="form-floating form-css">
@@ -17,32 +33,36 @@ const NewPlaceForm = () => {
 //     <input className="btn btn-primary" type="submit"/>
 //   </div>
 //       </form>
-<Form  className="form-css">
+<Form noValidate validated = {validated} onSubmit = {handleSubmit} className="form-css">
 <FloatingLabel
 controlId="floatingInput"
 label="Enter TITLE"
 className="mb-3"
 >
-<Form.Control type="text" placeholder="Your Title" />
+<Form.Control minLength={5} required type="text" placeholder="Your Title" />
+<Form.Control.Feedback type="invalid" >Please Fill this field / Enter Atleast 5 Characters</Form.Control.Feedback>
 </FloatingLabel>
 <FloatingLabel
 controlId="floatingInput"
 label="Enter Description"
 className="mb-3"
 >
-<Form.Control as="textarea" rows={5} placeholder = "Description Here" />
+<Form.Control required as="textarea" rows={5} placeholder = "Description Here" />
+<Form.Control.Feedback type="invalid" >Please Fill this field</Form.Control.Feedback>
 </FloatingLabel>
 <FloatingLabel
 controlId="floatingInput"
 label="Enter Address"
 className="mb-3"
 >
-<Form.Control type = "text" placeholder = "Address Here" />
+<Form.Control required type = "text" placeholder = "Address Here" />
+<Form.Control.Feedback type="invalid" >Please Fill this field</Form.Control.Feedback>
 </FloatingLabel>
 
-<Form.Control type = "file" placeholder = "File Here" accept="image/png, image/gif, image/jpeg" />
+<Form.Control required type = "file" placeholder = "File Here" accept="image/png, image/gif, image/jpeg" />
+<Form.Control.Feedback type="invalid" >Need Image</Form.Control.Feedback>
 <div className="d-grid gap-2 form-button-lg">
-<Button variant="secondary" size="lg">
+<Button type = "submit"  disabled = {false} variant="secondary" size="lg">
         Add New Picture
       </Button>
 </div>
